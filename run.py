4c2b809970559846e878711cad1de827ffa260eb
BenD2525 <bendawson2525@gmail.com>
2022-05-18 16:21:19 +0000

    Add to welcome message, import time module and add time.sleep between print statements

--- run.py
+++ run.py
@@ -1,13 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import time
+
+num_seconds = 1
 print("Welcome to Noughts and Crosses!")
-username = input("Please tell me your name: ")
+time.sleep(num_seconds)
+print("Also called 'Tic Tac Toe', try and beat me (your computer) ")
+print("in a best of 5 games where the goal is to join")
+print("up to 3 noughts or crosses together before me!")
+num_seconds += 1
+time.sleep(num_seconds)
+username = input("Now, who am I talking to? ")
 print(f"Pleased to meet you {username}.")
+time.sleep(num_seconds)
+print("Nice to know the name of the person I'll beat!")
+time.sleep(num_seconds)
 
 
 def coin_toss():
-    choice = input("Let's see who goes first. Heads or Tails? ")
+    choice = input("Now, let's see who goes first. Heads or Tails? ")
     capitalize_choice = choice.capitalize()
     if capitalize_choice == "Heads":
         print("Heads it is")
@@ -15,9 +24,8 @@ def coin_toss():
         print("Tails it is")
     else:
         print("You need to choose 'Heads' or 'Tails'. Let's try again.")
-        
+        coin_toss()    
     print(capitalize_choice)
-    coin_toss()
-
+    
 
 coin_toss()
