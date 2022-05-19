44a5199c939cd984ba56f9b4559d9dcc63dcf210
BenD2525 <bendawson2525@gmail.com>
2022-05-19 06:52:50 +0000

    Add description to welcome_message and coin_toss functions, add them to new_game function

--- run.py
+++ run.py
@@ -1,21 +1,33 @@
 import time
 
-num_seconds = 1
-print("Welcome to Noughts and Crosses!")
-time.sleep(num_seconds)
-print("Also called 'Tic Tac Toe', try and beat me (your computer) ")
-print("in a best of 5 games where the goal is to join")
-print("up to 3 noughts or crosses together before me!")
-num_seconds += 1
-time.sleep(num_seconds)
-username = input("Now, who am I talking to? ")
-print(f"Pleased to meet you {username}.")
-time.sleep(num_seconds)
-print("Nice to know the name of the person I'll beat!")
-time.sleep(num_seconds)
+
+def welcome_message():
+    """
+    Prints welcome message to user, explains the game
+    and requests a username.
+    """
+    num_seconds = 1
+    print("Welcome to Noughts and Crosses!")
+    time.sleep(num_seconds)
+    print("Also called 'Tic Tac Toe', try and beat me (your computer) ")
+    print("in a best of 5 games where the goal is to join")
+    print("up to 3 noughts or crosses together before me!")
+    num_seconds += 1
+    time.sleep(num_seconds)
+    username = input("Now, who am I talking to? ")
+    print(f"Pleased to meet you {username}.")
+    time.sleep(num_seconds)
+    print("Nice to know the name of the person I'll beat!")
+    time.sleep(num_seconds)
 
 
 def coin_toss():
+    """
+    Requests user input to choose heads or tails,
+    validates their choice and prints it back to the user.
+    Uses a random choice to select heads or tails, records result
+    and displays a win or loss message
+    """
     choice = input("Now, let's see who goes first. Heads or Tails? ")
     capitalize_choice = choice.capitalize()
     if capitalize_choice == "Heads":
@@ -28,4 +40,6 @@ def coin_toss():
     print(capitalize_choice)
     
 
-coin_toss()
+def new_game():
+    welcome_message()
+    coin_toss()
