2b14dd29dcd0ed9c69439b1c46437f11e21c66d1
BenD2525 <bendawson2525@gmail.com>
2022-05-19 07:48:18 +0000

    Add coin_toss_outcome function

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import time
+import random
 
 
 def welcome_message():
@@ -6,8 +7,8 @@ def welcome_message():
     Prints welcome message to user, explains the game
     and requests a username.
     """
-    num_seconds = 1
     print("Welcome to Noughts and Crosses!")
+    num_seconds = 1
     time.sleep(num_seconds)
     print("Also called 'Tic Tac Toe', try and beat me (your computer) ")
     print("in a best of 5 games where the goal is to join")
@@ -23,6 +24,29 @@ def welcome_message():
 
 def coin_toss():
     """
+    Calculates a random number, 1 or 0, then assigns a 'heads'
+    or 'tails' result based on the result. Prints a message to
+    advise the user of the result.
+    """
+    result = round(random.randint(0, 1))
+    if result == 0:
+        coin_toss_result = "Heads"
+        print("The computer flicks the coin up...")
+        num_seconds = 1
+        time.sleep(num_seconds)
+        print(f"It's {coin_toss_result}")
+    elif result == 1:
+        coin_toss_result = "Tails"
+        print("The computer flicks the coin up...")
+        num_seconds = 1
+        time.sleep(num_seconds)
+        print(f"It's {coin_toss_result}")
+    else:
+        print("There's a glitch in the matrix, let's try again.")
+
+
+def coin_toss_choice():
+    """
     Requests user input to choose heads or tails,
     validates their choice and prints it back to the user.
     Uses a random choice to select heads or tails, records result
@@ -32,14 +56,32 @@ def coin_toss():
     capitalize_choice = choice.capitalize()
     if capitalize_choice == "Heads":
         print("Heads it is")
+        coin_toss()
     elif capitalize_choice == "Tails":
         print("Tails it is")
+        coin_toss()
     else:
         print("You need to choose 'Heads' or 'Tails'. Let's try again.")
-        coin_toss()    
+        coin_toss_choice()   
     print(capitalize_choice)
+
+
+def coin_toss_outcome():
+    """
+    Calculates who won the coin toss and prints a message for each
+    result.
+    """
+    if capitalize_choice == coin_toss_result:
+        print("You win- lucky guess...")
+    else:
+        print("I win- binary is best!")
     
 
 def new_game():
     welcome_message()
+    coin_toss_choice()
     coin_toss()
+    coin_toss_outcome()
+
+
+new_game()
