ec9613772acfa912103f1c3d3e9dc243d7562c61
BenD2525 <bendawson2525@gmail.com>
2022-06-02 09:08:40 +0000

    import spreadsheet and credentials

--- run.py
+++ run.py
@@ -1,5 +1,22 @@
 import time
 import random
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('Noughts and Crosses')
+
+results = SHEET.worksheet('Results')
+
+data = results.get_all_values()
 
 
 def welcome_message():
@@ -164,4 +181,3 @@ def new_game():
     coin_toss_outcome()
 
 
-computer_turn()
